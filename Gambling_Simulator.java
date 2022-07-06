5cc9ba87e41fa55fc828b6cd4bd2b7ba752af408
Kunalpatil009 <009kunalpatil@gmail.com>
2022-07-06 17:38:15 +0530

    Prblm_1 Solution Added

---
+++ Gambling_Simulator.java
@@ -0,0 +1,12 @@
+public class Gambling_Simulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambler Simulator");
+        System.out.println("Dear Guest, Pleas Read The Deeds");
+
+        int EveryDayStack = 100;
+        int BetForGame = 1;
+
+        System.out.println("Every Day Stack For Every Game : $" + EveryDayStack);
+        System.out.println("Bet Price For Every Game : $" + BetForGame);
+    }
+}
